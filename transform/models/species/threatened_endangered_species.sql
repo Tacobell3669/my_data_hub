a1eca4279b58dcc730b9213fb97b3773b0f4ca4f
Bacotell36 <ryan@fixdapp.com>
2022-12-29 09:18:17 -0500

    add naming edits to first model, create a second model

--- transform/models/species/threatened_endangered_species.sql
+++ transform/models/species/threatened_endangered_species.sql
@@ -11,10 +11,10 @@ with dated as (
 )
 
 , threatened_endangered as (
-    select "Common Name",
-           "Scientific Name",
-           "ESA Listing Status",
-           "ESA Listing Date"::date,
+    select "Common Name" as common_name,
+           "Scientific Name" as scientific_name,
+           "ESA Listing Status" as esa_listing_status,
+           "ESA Listing Date"::date as esa_listing_date,
            "Current Range Combined States" as states_found
     from dated where "ESA Listing Status" = 'Threatened' or "ESA Listing Status" = 'Endangered'
 )
