48259146f111a21f7ccb15994f1f038208b5737b
Bacotell36 <ryan@fixdapp.com>
2023-04-08 16:11:43 -0400

    finish us states map

--- notebook/jupyter_helpers/us_states.py
+++ notebook/jupyter_helpers/us_states.py
@@ -1,6 +1,7 @@
 import json
 import geojson
 import plotly.express as px
+import plotly.offline as po
 import psycopg2
 import os
 from dotenv import load_dotenv
@@ -24,8 +25,8 @@ cur = conn.cursor()
 cur.execute("select state, cumulative_count, listing_year from \
             analytics.cumulative_count_by_state order by listing_year;")
 
-over_time_counts = cur.fetchall()
-over_time_counts_df = pd.DataFrame(over_time_counts, columns=['state', 'species_listing_count', 'listing_year'])
+counts_over_time = cur.fetchall()
+counts_over_time_df = pd.DataFrame(counts_over_time, columns=['state', 'species_listing_count', 'listing_year'])
 
 state_codes = {
     'AL': 'Alabama',
@@ -87,30 +88,30 @@ state_codes = {
 }
 
 # Map the two-character codes to the full names of the states
-over_time_counts_df['state_code'] = over_time_counts_df['state']
+counts_over_time_df['state_code'] = counts_over_time_df['state']
 
 # Transform the count data into a format that can be used by Chloropleth
-over_time_counts_df['species_listing_count'] = over_time_counts_df['species_listing_count'].astype(float)
-over_time_counts_df['listing_year'] = over_time_counts_df['listing_year'].astype(str)
-over_time_counts_df.drop('state', axis=1, inplace=True)
+counts_over_time_df['species_listing_count'] = counts_over_time_df['species_listing_count'].astype(float)
+counts_over_time_df['listing_year'] = counts_over_time_df['listing_year'].astype(str)
+counts_over_time_df.drop('state', axis=1, inplace=True)
 
 # Create full data set of all states and all years
 years = pd.date_range(start='1967', end='2023', freq='Y')
 years = years.strftime('%Y')
-state_codes = over_time_counts_df['state_code'].unique()
+state_codes = counts_over_time_df['state_code'].unique()
 combinations = list(itertools.product(state_codes, years))
 
 # Combine existing DataFrame with the combined list of state codes and years
 df_new = pd.DataFrame(combinations, columns=['state_code', 'listing_year'])
-df_new = pd.merge(df_new, over_time_counts_df, how='outer', left_on=['state_code', 'listing_year'],
+df_new = pd.merge(df_new, counts_over_time_df, how='outer', left_on=['state_code', 'listing_year'],
 right_on=['state_code', 'listing_year'])
 df_new['species_listing_count'] = df_new.groupby(['state_code'])['species_listing_count'].fillna(method='ffill')
 
-# Create an over time Chloropleth map using Plotly and the dataframe we made
+# Create an animated Chloropleth map using Plotly and the dataframe we made
 over_time_fig = px.choropleth(df_new, geojson=geojson_us_states, locations='state_code',
                     locationmode="USA-states", color='species_listing_count', title='State Species Listing Counts - 2020',
                     color_continuous_scale='Viridis_r', scope='usa', range_color=(0,500), labels={'species_listing_count':'# of species listed'},
                     animation_frame='listing_year')
 
-# Display the maps
-over_time_fig.show()
+# Write the Choropleth map to an HTML file to be used in the data report
+po.plot(over_time_fig, filename='species_map.html')
